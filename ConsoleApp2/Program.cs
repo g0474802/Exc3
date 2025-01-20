38fe1f9173b28a5907ee131534ec51a42a51dd5f
trenf <trenf@DESKTOP-JB5DFJ5>
2025-01-19 22:17:34 -0600

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exc3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the principal amount of the loan in dollars?");
+            var loanAmount = int.Parse(Console.ReadLine());
+            Console.WriteLine("What is the interest rate? (input .05 for 5%)");
+            var interestRate = (Console.ReadLine());
+            Console.WriteLine("what is the period of the loan in years?");
+            var period = int.Parse(Console.ReadLine());
+            Console.WriteLine("The total interest of the loan is " + (loanAmount * period));
+        }
+    }
+}
